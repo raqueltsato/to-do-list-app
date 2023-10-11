f27b749c4061fc5f2e71353f77e1e9fa6b3c7c20
Raquel Sato <raquelsato1@gmail.com>
2023-10-11 18:49:40 -0300

    ci: remove owner from script

--- scripts/semantic_version.sh
+++ scripts/semantic_version.sh
@@ -4,14 +4,13 @@
 version_mode="patch"
 
 function get_semantic_version {
-    owner=${SEMANTIC_VERSION_GITHUB_OWNER}
     repo="to-do-list-app"
 
     response=$(curl -sL \
         -H "Accept: application/vnd.github.v3+json" \
         -H "Authorization: Bearer ${GITHUB_API_TOKEN}" \
         -H "X-GitHub-Api-Version: 2022-11-28" \
-        "https://api.github.com/repos/${owner}/${repo}/pulls?state=closed&sort=updated&direction=desc&page=1&per_page=1")
+        "https://api.github.com/repos/${SEMANTIC_VERSION_GITHUB_OWNER}/${repo}/pulls?state=closed&sort=updated&direction=desc&page=1&per_page=1")
 
     labels_names=$(echo "$response" | jq -r '.[0].labels[].name')
 
