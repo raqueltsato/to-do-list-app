b5e4b01e8f2c9d6479465316bea0191e02158f29
Raquel Sato <raquelsato1@gmail.com>
2023-10-11 18:23:43 -0300

    ci: add semantic version

---
+++ scripts/semantic_version.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+# Definindo o modo padrão da versão
+version_mode="patch"
+
+function get_semantic_version {
+    owner=${SEMANTIC_VERSION_GITHUB_OWNER}
+    repo="to-do-list-app"
+
+    response=$(curl -sL \
+        -H "Accept: application/vnd.github.v3+json" \
+        -H "Authorization: Bearer ${GITHUB_API_TOKEN}" \
+        -H "X-GitHub-Api-Version: 2022-11-28" \
+        "https://api.github.com/repos/${owner}/${repo}/pulls?state=closed&sort=updated&direction=desc&page=1&per_page=1")
+
+    labels_names=$(echo "$response" | jq -r '.[0].labels[].name')
+
+    if [[ "$labels_names" == *"major"* ]]; then
+        version_mode="major"
+    elif [[ "$labels_names" == *"minor"* ]]; then
+        version_mode="minor"
+    fi
+}
+
+get_semantic_version
+
+echo "Generating version..."
+echo "Labels: ${labels_names}"
+npm version $version_mode
+echo "Version generated $version_mode."
