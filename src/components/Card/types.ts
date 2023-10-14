c8e990c9e1c1bb67a7417a6f86670728e4ecc74e
Raquel Sato <raquelsato1@gmail.com>
2023-10-14 14:30:55 -0300

    perf: fix re-rendering card

--- src/components/Card/types.ts
+++ src/components/Card/types.ts
@@ -1,7 +1,9 @@
+import { TFunction } from "i18next";
 import { ToDoType } from "../../contexts/toDoContext/types";
 
 export type OwnProps = {
   toDo: ToDoType;
   handleDoneToDo: (id: string) => void;
   handleRemoveToDo: (id: string) => void;
+  t: TFunction<"locale", undefined>;
 };
